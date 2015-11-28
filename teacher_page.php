0b4e2780bb906a45fba0752337c2b6e9d6e573d1
YoshiokaYuuki <yuuki1@YuukiMacbookAir.local>
2015-11-28 17:23:28 +0900

    test

---
+++ teacher_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Kidsカルテ</title>
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<frameset cols="20%,*,20%">
+
+<frame src="teacher_page1.php">
+<frame src="teacher_page2.php">
+<frame src="teacher_page3.php">
+
+</frameset>
+
+</html>
