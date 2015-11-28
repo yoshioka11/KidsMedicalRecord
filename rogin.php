d813ef89d3da9140bb71016bfd35e7d1400fc455
naotsukamoto <m0naaa0u@gmail.com>
2015-11-28 17:08:32 +0900

    ログインページ

---
+++ rogin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>ログインページ(先生用)</title>		
+	</head>
+	<body>
+		<h1>ログインページ</h1>
+
+		<!-- ページの連携 -->
+		<form action="teacher_page.php" method="post">
+		<!-- idの入力フォーム -->
+		<input type="text" name="id" value=""></br></br>
+		<!-- 先生の名前の入力フォーム -->
+		<input type="text" name="name" value=""></br>
+		<!-- 送信ボタン -->
+		<input type="submit" value="送信">
+	</form>
+	</body>
+</html>
