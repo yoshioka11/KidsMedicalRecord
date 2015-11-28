0e1e397525519ad57398ee1f8aaa53b096ddab22
naotsukamoto <m0naaa0u@gmail.com>
2015-11-28 17:25:36 +0900

    ログインpage

--- rogin.php
+++ rogin.php
@@ -2,7 +2,8 @@
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<title>ログインページ(先生用)</title>		
+	<title>ログインページ(先生用)</title>	
+	<link rel="stylesheet" href="rogin.css">	
 	</head>
 	<body>
 		<h1>ログインページ</h1>
@@ -10,11 +11,13 @@
 		<!-- ページの連携 -->
 		<form action="teacher_page.php" method="post">
 		<!-- idの入力フォーム -->
+		ID :
 		<input type="text" name="id" value=""></br></br>
 		<!-- 先生の名前の入力フォーム -->
+		Name : 
 		<input type="text" name="name" value=""></br>
 		<!-- 送信ボタン -->
-		<input type="submit" value="送信">
+		<input id="submit" type="submit" value="送信">
 	</form>
 	</body>
 </html>
