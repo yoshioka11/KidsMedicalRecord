8886cc68087da13c835e1f71e25f500de2ae1a4a
YoshiokaYuuki <yuuki1@YuukiMacbookAir.local>
2015-11-29 09:13:25 +0900

    競合の修正

--- login.php
+++ login.php
@@ -11,12 +11,9 @@
 		<!-- ページの連携 -->
 		<form action="login_check.php" method="post">
 		<!-- idの入力フォーム -->
-		ID : 
-<<<<<<< HEAD
+		ID :
 		<input type="text" name="id" value="<?php $_SESSION['kids_name']?>"></br></br>
-=======
 		<input type="text" name="t_id" value=""></br></br>
->>>>>>> 27ced5d6e75f995080581ac6e69873827ddb80e1
 		Password : 
 		<input type="password" name="t_pass" value=""></br>
 		<!-- 送信ボタン -->
